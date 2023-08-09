dab729b7b9cc2e8b55c7a431ad9a9f15c0c9dae3
alvarodc99 <109382487+alvarodc99@users.noreply.github.com>
2023-08-08 23:35:45 -0300

    Update products.js

--- js/products.js
+++ js/products.js
@@ -1,24 +1,3 @@
-// const autos101 = 'https://japceibal.github.io/emercado-api/cats_products/101.json';
-
-// const contenedor = document.getElementsByClassName('producto');
-
-// function mostrarProducto(dataArray){
-//     for (const item of dataArray){
-//         contenedor.innerHTML += <p> $ {item.name} $ {item.description} </p>
-//     }
-// }
-
-
-// fetch(autos101)
-//   .then(response => response.json())
-//   .then(data => {
-//     mostrarProducto(data.products);
- 
-//   });
-
-
-
-
 const autos101 = 'https://japceibal.github.io/emercado-api/cats_products/101.json';
 
 const contenedor = document.getElementsByClassName('producto')[0]; 
